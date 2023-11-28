540f13022d8520648fb6b61878174d12406cff5f
akocoglu <ahmetkocoglu@outlook.com>
2023-11-28 21:47:11 +0300

    remove middleware

--- crm-frontend/src/store/index.ts
+++ crm-frontend/src/store/index.ts
@@ -1,14 +1,12 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
 // ** Reducers
-
 import login from '@/store/apps/login'
 
 export const store = configureStore({
     reducer: {
         login
-    },
-    middleware: getDefaultMiddleware => getDefaultMiddleware({serializableCheck: false})
+    }
 })
 
 export type AppDispatch = typeof store.dispatch
