78fc9eb9cc80b1ebebf8d014d063568f9a71cda6
emre <yunuzemre30@gmail.com>
2023-12-18 21:43:37 +0100

    edit-18-12

--- crm-backend/src/enum/TaskStatus.ts
+++ crm-backend/src/enum/TaskStatus.ts
@@ -1,1 +1,5 @@
 export enum TaskStatus { APPOINTED = 'appointed', IN_PROGRESS = 'in_progress', DONE = 'done' };
+
+//appointed: anlamı: atanmış
+//in_progress: anlamı: devam ediyor
+//done: anlamı: tamamlandı
--- crm-backend/src/enum/UserConfirmEnum.ts
+++ crm-backend/src/enum/UserConfirmEnum.ts
@@ -1,3 +1,8 @@
 
 
 export enum UserConfirmEnum{ PENDING = 'pending', EMAIL = 'email', APPROVAL = 'approval', DENIED = 'denied' };
+
+// pending: anlamı: bekliyor
+// email: anlamı: eposta
+// approval: anlamı: onay
+// denied: anlamı: reddedildi
