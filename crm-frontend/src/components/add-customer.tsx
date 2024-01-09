9300f9d44cd509ef3ad1e92a240e3818c4ee5af8
akocoglu <ahmetkocoglu@outlook.com>
2024-01-09 10:05:29 +0300

    new customer

--- crm-frontend/src/components/add-customer.tsx
+++ crm-frontend/src/components/add-customer.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from "react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 // import { useDispatch } from 'react-redux';
 // import { AppDispatch } from '@/store';
-import { useForm } from 'react-hook-form';
-import Input from './input';
-import { addUser } from '@/store/apps/user';
+import { useForm } from "react-hook-form";
+import Input from "./input";
+import { addUser } from "@/store/apps/user";
+import { useSetUserMutation } from "@/services/user";
 
 type FormValues = {
   firstName: string;
@@ -26,8 +27,9 @@ const defaultValues: FormValues = {
 };
 
 const AddCustomer = () => {
-  // ** Redux
-  // const dispatch = useDispatch<AppDispatch>();
+  const [setUser] = useSetUserMutation();
+  // ** State
+  const [loading, setLoading] = useState(false)
 
   const {
     register,
@@ -41,53 +43,63 @@ const AddCustomer = () => {
   });
 
   const onSubmit = (payload: FormValues) => {
-    // dispatch(addUser(payload));
+    setLoading(true)
+    setUser(payload)
+      .unwrap()
+      .then(() => {
+        console.log("User added");
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log("Failed to add user");
+        setLoading(false)
+      });
     reset(defaultValues);
   };
 
   return (
     <>
-    <main className="max-w-2xl mx-auto">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-wrap -mx-4 py-28 gap-y-2">
-          <div className="w-full md:w-1/1 px-1">
-            <Input
-              type="text"
-              placeholder="First Name"
-              className="mt-1"
-              rounded="rounded-2xl"
-              {...register("firstName", { required: true })}
-            />
-            {errors.firstName && <>{errors.firstName.message}</>}
+      <main className="max-w-2xl mx-auto">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-wrap -mx-4 py-28 gap-y-2">
+            <div className="w-full md:w-1/1 px-1">
+              <Input
+                type="text"
+                placeholder="First Name"
+                className="mt-1"
+                rounded="rounded-2xl"
+                {...register("firstName", { required: true })}
+              />
+              {errors.firstName && <>{errors.firstName.message}</>}
+            </div>
+            <div className="w-full md:w-1/1 px-1">
+              <Input
+                type="text"
+                placeholder="Last Name"
+                className="mt-1"
+                rounded="rounded-2xl"
+                {...register("lastName", { required: true })}
+              />
+              {errors.lastName && <>{errors.lastName.message}</>}
+            </div>
+            <div className="w-full md:w-1/1 px-1">
+              <Input
+                type="text"
+                placeholder="Email"
+                className="mt-1"
+                rounded="rounded-2xl"
+                {...register("email", { required: true })}
+              />
+              {errors.email && <>{errors.email.message}</>}
+            </div>
+            <div className="w-full md:w-1/1 px-5 text-end">
+              <button type="submit" disabled={loading}>{loading ? 'işleminiz yapılıyor' : 'Gönder'}</button>
+            </div>
           </div>
-          <div className="w-full md:w-1/1 px-1">
-            <Input
-              type="text"
-              placeholder="Last Name"
-              className="mt-1"
-              rounded="rounded-2xl"
-              {...register("lastName", { required: true })}
-            />
-            {errors.lastName && <>{errors.lastName.message}</>}
-          </div>
-          <div className="w-full md:w-1/1 px-1">
-            <Input
-              type="text"
-              placeholder="Email"
-              className="mt-1"
-              rounded="rounded-2xl"
-              {...register("email", { required: true })}
-            />
-            {errors.email && <>{errors.email.message}</>}
-          </div>
-          <div className="w-full md:w-1/1 px-5 text-end">
-            <button type="submit">Gönder</button>
-          </div>
-        </div>
-      </form>
+        </form>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default AddCustomer
+export default AddCustomer;
