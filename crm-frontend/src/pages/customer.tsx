75827b0b2a57da5ea337e38d80d83f2210cc0b1e
akocoglu <ahmetkocoglu@outlook.com>
2024-01-09 10:27:37 +0300

    customers

--- crm-frontend/src/pages/customer.tsx
+++ crm-frontend/src/pages/customer.tsx
@@ -1,30 +1,58 @@
-import Menu from '@/components/menu'
+import Menu from "@/components/menu";
+import { useGetUsersQuery } from "@/services/user";
 // import { AppDispatch, RootState } from '@/store'
-import { getUsers } from '@/store/apps/user'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { getUsers } from "@/store/apps/user";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Customer = () => {
-  // ** Redux **
-  // const dispatch = useDispatch<AppDispatch>();
-
-  // ** Selector **
-  // const usersLoading = useSelector((state: RootState) => state.user.usersLoading)
-  // const users: any[] = useSelector((state: RootState) => state.user.users)
-
-  // useEffect(() => {
-  //   dispatch(getUsers())
-  // }, [dispatch])
-  
+  const { data, error, isLoading } = useGetUsersQuery("/users");
 
   return (
     <>
       <Menu />
-      {/* {usersLoading ? "Yükleniyor" : users.map((k:any) =>{
-        return <><div>{k.firstName} {k.lastName} {k.email} {k.role}</div></>
-      })} */}
+      <div>Müşteriler Sayfası</div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Adı</th>
+              <th>Soyadı</th>
+              <th>E-Posta</th>
+              <th>Rol</th>
+            </tr>
+          </thead>
+          <tbody>
+          {isLoading
+        ? "Yükleniyor"
+        : data.data?.map((k: any) => {
+            return (
+              <>
+              <tr>
+                <td></td>
+                <td>
+                  {k.firstName}
+                </td>
+                <td>
+                  {k.lastName}
+                </td>
+                <td>
+                  {k.email}
+                </td>
+                <td>
+                  {k.role}
+                </td>
+              </tr>
+              </>
+            );
+          })}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Customer
+export default Customer;
